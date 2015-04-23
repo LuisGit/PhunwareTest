e41da4ac5f083271227436dec7c5d45242daf6de
Luis Alvarado <luisgas@gmail.com>
2015-04-23 16:51:30 -0600

    Updating values names and types.

--- PhunwareTest/class/api/APIManager.m
+++ PhunwareTest/class/api/APIManager.m
@@ -11,37 +11,24 @@
 #import "Venue.h"
 #import "JSONModel.h"
 
-#define kBaseURLString @"https://s3.amazonaws.com/jon-hancock-phunware/"
+const NSString *kBaseURLString = @"https://s3.amazonaws.com/jon-hancock-phunware/";
 
 @implementation APIManager
 
 +(void)retrieveVenuesFromURL:(NSString *)stringURL success:(successBlock)successBlock failure:(failureBlock)failureBlock{
-    NSString *string = [NSString stringWithFormat:@"%@%@", kBaseURLString, stringURL];
-    NSURL *url = [NSURL URLWithString:string];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSString *fullStringURL = [NSString stringWithFormat:@"%@%@", kBaseURLString, stringURL];
     
-    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
-    [operation.responseSerializer.acceptableContentTypes setByAddingObject:@"text/plain"];
-    operation.responseSerializer = [AFHTTPResponseSerializer serializer];
-    
-    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
-        //Get feed to string.
-        NSString *string = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
-        
-        //Encode string for non standard /exit characters
-        NSData *jsonData = [string dataUsingEncoding:NSUTF8StringEncoding];
-       
-        //Parse Json feed.
-        NSArray *venuesArray  = [Venue arrayOfModelsFromData:jsonData error:nil];
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/plain"];
+    [manager GET:[NSString stringWithFormat:fullStringURL ,kBaseURLString] parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        //Parse Json Feed.
+        NSArray *venuesArray = [Venue arrayOfModelsFromDictionaries:responseObject error:nil];
         successBlock(venuesArray);
         
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         //Send error message
         failureBlock(error);
     }];
-    
-    //Start feed download
-    [operation start];
 }
 
 @end
